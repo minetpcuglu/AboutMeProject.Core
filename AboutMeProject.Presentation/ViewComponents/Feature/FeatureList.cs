40c14d766b4659a32526b07c10ba45237cd95247
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-04-25 00:39:21 +0300

    25.04 / FeatureList VC's

--- AboutMeProject.Presentation/ViewComponents/Feature/FeatureList.cs
+++ AboutMeProject.Presentation/ViewComponents/Feature/FeatureList.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AboutMeProject.Application.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,19 @@ namespace AboutMeProject.Presentation.ViewComponents.Feature
 {
     public class FeatureList : ViewComponent
     {
-
-       public IViewComponentResult Invoke()
+        private readonly IFeatureService _featureService;
+        //private readonly IValidator<EducationVM> _educationValidator;
+        public FeatureList(IFeatureService featureService/*, IValidator<EducationVM> educationValidator*/)
         {
-
-            return View();
+            //_educationValidator = educationValidator;
+            _featureService = featureService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var itemsTask = GetItemsAsync();
+            var value = await _featureService.GetAll();
+            //var value =  await _featureService.GetAll();
+            return View(value);
         }
     }
 }
