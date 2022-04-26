38a4f0ead69fdec7cae2d9bb5941f391aa07717c
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-04-26 04:51:57 +0300

    26.04 / Education

---
+++ AboutMeProject.Presentation/ViewComponents/Education/EducationList.cs
@@ -0,0 +1,25 @@
+﻿using AboutMeProject.Application.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Presentation.ViewComponents.Education
+{
+    public class EducationList : ViewComponent
+    {
+        private readonly IEducationService _educationService;
+        //private readonly IValidator<EducationVM> _educationValidator;
+        public EducationList(IEducationService educationService/*, IValidator<EducationVM> educationValidator*/)
+        {
+            //_educationValidator = educationValidator;
+            _educationService = educationService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var value = await _educationService.GetAll();
+            return View(value);
+        }
+    }
+}
