14ebb8c1d3920764fff2f47a20a2c5ea02453a1f
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-04-29 02:47:42 +0300

    29.04 / Admin Layout Revize

---
+++ AboutMeProject.Presentation/Controllers/SkillController.cs
@@ -0,0 +1,30 @@
+﻿using AboutMeProject.Application.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Presentation.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly ISkillService _skillService;
+        //private readonly IValidator<EducationVM> _educationValidator;
+        public SkillController(ISkillService skillService/*, IValidator<EducationVM> educationValidator*/)
+        {
+            //_educationValidator = educationValidator;
+            _skillService = skillService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async  Task<IActionResult> GetList()
+        {
+            var value = await _skillService.GetAll();
+            return View(value);
+        }
+    }
+}
