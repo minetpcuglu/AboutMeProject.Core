22001bfa85ba1a1891573cc4bb0ddddf168849ed
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-05-01 23:42:59 +0300

    01.05 / Revize

---
+++ AboutMeProject.Presentation/ViewComponents/Dashboard/FeatureStatistic.cs
@@ -0,0 +1,25 @@
+﻿using AboutMeProject.Application.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Presentation.ViewComponents.Dashboard
+{
+    public class FeatureStatistic : ViewComponent
+    {
+        private readonly IFeatureService _featureService;
+    
+        public FeatureStatistic(IFeatureService featureService)
+        {
+         
+            _featureService = featureService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var value = await _featureService.GetAll();
+            return View(value);
+        }
+    }
+}
