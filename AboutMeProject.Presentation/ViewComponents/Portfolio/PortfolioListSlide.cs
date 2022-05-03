23f36cf5f15b7e47738ddd2e9891c2cc064477d1
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-05-04 01:52:26 +0300

    04.05/ VC's

---
+++ AboutMeProject.Presentation/ViewComponents/Portfolio/PortfolioListSlide.cs
@@ -0,0 +1,26 @@
+﻿using AboutMeProject.Application.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Presentation.ViewComponents.Portfolio
+{
+    public class PortfolioListSlide : ViewComponent
+    {
+        private readonly IPortfolioService _portfolioService;
+
+        public PortfolioListSlide(IPortfolioService portfolioService)
+        {
+
+            _portfolioService = portfolioService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var value = await _portfolioService.GetAll();
+            return View(value);
+        }
+    }
+}
