672201e6ff96108f034bd98302f58a911684c5bf
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-05-03 22:04:19 +0300

    03.05 / VC's

---
+++ AboutMeProject.Presentation/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,26 @@
+﻿using AboutMeProject.Application.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Presentation.ViewComponents.Dashboard
+{
+    public class MessageList:ViewComponent
+    {
+        private readonly IMessageService _messageService;
+
+        public MessageList(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var value = await _messageService.GetAll();
+
+            return View(value);
+        }
+    }
+}
