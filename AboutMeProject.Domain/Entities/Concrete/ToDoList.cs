fb8058cd875705e00698b1f684e88d41002e995c
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-05-04 02:00:04 +0300

    04.05 / ToDoList Entity Code First

---
+++ AboutMeProject.Domain/Entities/Concrete/ToDoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Domain.Entities.Concrete
+{
+   public class ToDoList : BaseEntity<int>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
--- AboutMeProject.Infrastructure/Context/ApplicationDbContext.cs
+++ AboutMeProject.Infrastructure/Context/ApplicationDbContext.cs
@@ -23,6 +23,7 @@ namespace AboutMeProject.Infrastructure.Context
         public DbSet<Portfolio> Portfolios { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Skill> Skills { get; set; }
+        public DbSet<ToDoList> ToDoLists { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserMessage> userMessages { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
