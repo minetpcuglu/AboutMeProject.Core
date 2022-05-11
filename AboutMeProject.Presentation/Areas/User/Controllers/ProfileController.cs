66e6b45d28efc846db7bb04993710d7362b6cf7e
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-05-11 10:57:51 +0300

    11/05 Profile Page

---
+++ AboutMeProject.Presentation/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Presentation.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ProfileController : Controller
+    {
+        //private readonly UserManager<>
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+
+        //public IActionResult EditProfile(string p)
+        //{
+        //    return View();
+        //}
+    }
+}
