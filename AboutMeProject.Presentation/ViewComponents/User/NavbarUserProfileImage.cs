8e330820ac05b69a38b22a148e2e2355b49549c6
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-05-14 12:49:34 +0300

    14.05/ Navbar

---
+++ AboutMeProject.Presentation/ViewComponents/User/NavbarUserProfileImage.cs
@@ -0,0 +1,33 @@
+﻿using AboutMeProject.Domain.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMeProject.Presentation.ViewComponents.User
+{
+    public class NavbarUserProfileImage : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+       
+
+        public NavbarUserProfileImage(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var value = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.Image = value.ImageUrl;
+
+            var valueId = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.UserId = value.Id;
+
+            return View();
+        }
+    }
+}
