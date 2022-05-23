741a604f5a5e0c5e6dd2d2f030efd3fcad4f3a08
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-05-23 20:02:20 +0300

    23.05 / Js

---
+++ AboutMeProject.Presentation/wwwroot/Createjs/Contact.js
@@ -0,0 +1,19 @@
+﻿function submitForm() {
+    var data = $("#ContactForm").serialize();
+    console.log(data);
+    alert(data);
+    $.ajax({
+        type: 'POST',
+        url: '/Default/CreateContact',
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8', // when we use .serialize() this generates the data in query string format. this needs the default contentType (default content type is: contentType: 'application/x-www-form-urlencoded; charset=UTF-8') so it is optional, you can remove it
+        data: data,
+        success: function (result) {
+            alert('Successfully received Data ');
+            console.log(result);
+        },
+        error: function () {
+            alert('Failed to receive the Data');
+            console.log('Failed ');
+        }
+    })
+}
